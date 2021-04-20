61a6240159c996378238562631729bb1571ed526
Daniel Saldivar <daniel.saldivar.ds@gmail.com>
2021-04-19 21:43:30 -0400

    fixed test

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,6 +10,6 @@ runner = CliRunner()
 
 
 def test_app():
-    result = runner.invoke(app, ["--name", "Camila"])
+    result = runner.invoke(app, ["Camila"])
     assert result.exit_code == 0
     assert "Hello Camila" in result.stdout
